b34cf2442d6e139bf58d2dfe153c343339c44843
arunnvd <arun.nvd@gmail.com>
2020-02-16 20:31:47 +0300

    Display all images in folder

--- opencvTest.py
+++ opencvTest.py
@@ -1,34 +1,49 @@
 import numpy as np
 import cv2
 import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-img = cv2.imread('images/45bcfb5e-8b25-4d83-9c6b-61fc39b42d63.png', -1)
+def img_load(dir):
+    image_arr = []
+    for file in os.listdir(dir):
+        if file.endswith('png') or file.endswith('jpg') or file.endswith('jpeg'):
+            rawimg = cv2.imread(os.path.join(dir, file))
+            if rawimg is not None:
+                image_arr.append(rawimg)
+    return image_arr
+                             
+#img = cv2.imread('images/45bcfb5e-8b25-4d83-9c6b-61fc39b42d63.png', -1)
 classifier = cv2.CascadeClassifier('/home/pi/.local/lib/python3.5/site-packages/cv2/data/haarcascade_frontalface_default.xml')
 
-img_width, img_heigth = img.shape[:2]
+img_arr = []
+img_arr = img_load(os.path.join(BASE_DIR,'images'))
 
-if img_heigth > 500 and img_width > 500:
-    scale_per = 30
-else:
-    scale_per = 100
+for img in img_arr:
+    img_width, img_heigth = img.shape[:2]
+    
+    print(img_width, img_heigth)
 
-width = int(img.shape[1] * scale_per/100)
-heigth = int(img.shape[0] * scale_per/100)
-dim = (width , heigth)
-resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    if img_heigth > 500 or img_width > 500:
+        scale_per = 30
+    else:
+        scale_per = 100
 
-print(resized.shape)
+    width = int(img.shape[1] * scale_per/100)
+    heigth = int(img.shape[0] * scale_per/100)
+    dim = (width , heigth)
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-faces = classifier.detectMultiScale(resized)
+    faces = classifier.detectMultiScale(resized)
 
-for box in faces:
-    x,y,w,h = box
-    cv2.rectangle(resized, (x,y),(x+w, y+h), (255,0,0), 2) 
+    for box in faces:
+        x,y,w,h = box
+        cv2.rectangle(resized, (x,y),(x+w, y+h), (255,0,0), 2) 
 
-cv2.imshow('ArunTestWindow', resized)
-#cv2.imwrite('images/resized_1.jpg' , resized)
+    window = 'Test' + str(img.shape[0])
+    cv2.imshow(window, resized)
+
 key = cv2.waitKey(0)
-print("face = " , faces)
+#print("face = " , faces)
 
 cv2.destroyAllWindows()
 
