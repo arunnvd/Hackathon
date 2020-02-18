b037ac6cb33cf857e2e284c1dc1a6b46cca6539a
arunnvd <arun.nvd@gmail.com>
2020-02-18 22:58:00 +0530

    Saving faces if captured morethan threshold number frames

--- camera.py
+++ camera.py
@@ -1,19 +1,43 @@
 import cv2
 import numpy as np
+import os
 
 
 cap = cv2.VideoCapture(0)
-
 classifier = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_alt2.xml')
-
-def store_detected_frame(x,y,w,h):
-    print('it is working')
-    return
 
 face_rect_sum = 0
 FALSE_DETECTION_THRESHOLD = 50
+NEEDED_FRAMES = 20
 ITRATION_THRESHOLD = 3
 false_iteration = 0
+image_buff = []
+person_id = 0
+
+def clean_image_buff():
+    image_buff.clear()
+    print('clean buff', image_buff)
+
+def store_detected_framebuffer(roi):
+    print('Saving --> ',roi.shape, ' -- ',len(image_buff))
+    image_buff.append(roi)
+    return
+
+def save_person():
+    print('save Frames')
+    global person_id
+    frame_dir = 'detected_faces/personId_' + str(person_id)
+    if (os.path.isdir(frame_dir) == False):
+        os.mkdir(frame_dir)
+
+    for i,roi in enumerate(image_buff,start=0):
+        frame_name = frame_dir +'/frame_' + str(i) + '.jpg'
+        cv2.imwrite(frame_name,roi)
+    
+    #clean image buffer, increment person id
+    clean_image_buff()
+    person_id += 1
+
 while(True):
     ret, frame = cap.read()
     face_detected = False
@@ -35,21 +59,28 @@ while(True):
             face_rect_sum = curr_sum
             false_iteration = 0
             #start storring images to buffer
-            print('store this frame to buffer')
+            store_detected_framebuffer(gray[y:y+h, x:x+w])
+            #print('store this frame to buffer')
         else:
-            print('False Detection ')
+            #print('False Detection ')
             false_iteration += 1
             if false_iteration >= ITRATION_THRESHOLD:
                 print('reset buffer, and start fresh')
                 face_rect_sum = 0
                 #remove previous images and start fresh
+                clean_image_buff()
 
     if(face_detected == False):
         false_iteration += 1
         if false_iteration >= ITRATION_THRESHOLD :
+            #save buffer if buffered more than NEEDED_FRAMES
+            if(len(image_buff) >= NEEDED_FRAMES):
+                save_person()
             face_rect_sum = 0
             print('reset buffer, and start fresh due to false detection')
             #remove previous images and start fresh
+            clean_image_buff()
+
                 
 
     print(face_detected, face_rect_sum)
