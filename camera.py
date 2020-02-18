f1275473d5c1f243944636750ed51ef7611e6845
arunnvd <arun.nvd@gmail.com>
2020-02-18 20:54:55 +0530

    Revert "Update camera.py"
    This reverts commit 19b70396e6b5e1f1d0c9793be0aaba447f61d610.

--- camera.py
+++ camera.py
@@ -4,47 +4,20 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-classifier = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_alt2.xml')
+classifier = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
 
-def read_person_activity(x,y,w,h):
-    print('it is working')
-    return
-
-face_rect_sum = 0
-false_detection_threshold = 50
 while(True):
     ret, frame = cap.read()
-    face_detected = False
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = classifier.detectMultiScale(gray,scaleFactor=1.6, minNeighbors=6)
+    faces = classifier.detectMultiScale(gray,1.3,5)
 
     for (x,y,w,h) in faces:
-        face_detected = True
-        curr_sum = 0
         cv2.rectangle(frame,(x,y),(x+w, y+h), (255,0,0),2)
-        curr_sum = x + y + w + h
-        print('curr = ',curr_sum,'face_sum = ',face_rect_sum,'diff = ',face_rect_sum - curr_sum)
-        if face_rect_sum == 0:
-            face_rect_sum = curr_sum
-        elif abs(face_rect_sum - curr_sum) < false_detection_threshold:
-            face_rect_sum = curr_sum
-        else:
-            print('False Detection ')
-            face_rect_sum = 0
-            
-
-    print(face_detected, face_rect_sum)
 
     cv2.imshow('test', frame)
-
-
-
-
-
-    key = cv2.waitKey(500)
+    key = cv2.waitKey(10)
     if key ==32:
         break
-
 cap.release()
 cv2.destroyAllWindows()
