ffd579291eee3e1fcf4187e8e5a688a8226e6ef4
arunnvd <38983682+arunnvd@users.noreply.github.com>
2020-03-10 17:58:53 +0530

    Adding trained Classifier

--- personfinder.py
+++ personfinder.py
@@ -1,50 +1,66 @@
 import cv2
 import face_recognition
 import numpy as np
-#from sklearn import svm
+from sklearn import svm
+import os
+
+SAMPLES_PATH = 'detected_faces/'
+encodings = []
+person_id = []
+clf = svm.SVC(gamma = 'scale')
 
 
 video = cv2.VideoCapture(0)
-person1 = face_recognition.load_image_file("detected_faces/personId_3/bharath.jpg")
-person1_encoding = face_recognition.face_encodings(person1)[0]
 
-person2 = face_recognition.load_image_file("detected_faces/personId_2/frame_2.jpg")
-person2_encoding = face_recognition.face_encodings(person2)[0]
+def face_train(dir_path):
+    global encodings
+    global person_id
+    train_dir = os.listdir(dir_path);
 
-known_face_encodings = [
-    person1_encoding,
-    person2_encoding
-]
-known_face_names = [
-    "Bharath",
-    "Arun"
-]
+    for person in train_dir:
+        sample_pix = os.listdir(dir_path + person)
 
+        for samples in sample_pix:
+            face = face_recognition.load_image_file(dir_path + person + '/' + samples)
+
+            face_enc = face_recognition.face_encodings(face)[0]
+            encodings.append(face_enc)
+            person_id.append(person)
+    
+    clf.fit(encodings, person_id)
+
+def face_detect(rgb_frame):
+    name = ''
+    test_face_locations = face_recognition.face_locations(rgb_frame)
+    num_faces = len(test_face_locations)
+    for i in range(num_faces):
+        test_face_encodings = face_recognition.face_encodings(rgb_frame, test_face_locations)[i]
+        name = clf.predict([test_face_encodings])
+    print("----------Found---------------")
+    print(name)
+    print("----------Found-end--------------")
+    return name
+
+
+
+face_train(SAMPLES_PATH)
 while True:
     ret, frame = video.read()
     rgb_frame = frame[:, :, ::-1]
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        matches = face_recognition.compare_faces(known_face_encodings,face_encoding,tolerance=0.43)
-        print(matches)
+    person_id = face_detect(rgb_frame)
 
-        name = "Unknown"
+    if (not person_id):
+        print("Nothing found")
+    else:
+        face_locations = face_recognition.face_locations(rgb_frame)
 
-        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        for (top, right, bottom, left) in face_locations:
 
-        print(face_distances)
-
-        best_match_index = np.argmin(face_distances)
-
-        if matches[best_match_index]:
-            name = known_face_names[best_match_index]
-        
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, person_id[0], (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
     
     cv2.imshow('Video', frame)
 
